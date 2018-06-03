31fb956a0e11424a3143d56b3a793e1d2ca934e6
Alexander Ross <alex@aross.se>
2018-06-03 22:37:48 +0200

    Part 1

--- URLViewer/ViewController.swift
+++ URLViewer/ViewController.swift
@@ -1,25 +1,8 @@
-//
-//  ViewController.swift
-//  URLViewer
-//
-//  Created by Alexander Ross on 2018-06-03.
-//  Copyright © 2018 Alexander Ross. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    @IBOutlet weak var urlLabel: UILabel!
 
 }
 
